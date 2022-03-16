40b02e61be0c93ccc13c409c248505f0b21593a3
Hongbusi <q951686662@qq.com>
2022-03-16 19:23:38 +0800

    chore: useClipboard

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <p>isSupported: {{ isSupported }}</p>
   <p>Current copied: {{ text || 'none' }}</p>
   <input v-model="source" type="text">
-  <button @click="handleClickCopy(source)">
+  <button @click="copy">
     <!-- by default, `copied` will be reset in 1.5s -->
     <span v-if="!copied">Copy</span>
     <span v-else>Copied!</span>
@@ -13,5 +13,5 @@
 import { useClipboard } from '@vueuse/core'
 
 const source = ref('Hongbusi')
-const { text, copy: handleClickCopy, copied, isSupported } = useClipboard({ source })
+const { text, copy, copied, isSupported } = useClipboard({ source })
 </script>
